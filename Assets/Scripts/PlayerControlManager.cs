fdb466ad7ed44f77b170cfc7bd320f1c9abe9193
Yuri1427934 <111900757+Yuri1427934@users.noreply.github.com>
2023-09-19 17:42:23 -0600

    add sabotage and interact input

--- Assets/Scripts/PlayerControlManager.cs
+++ Assets/Scripts/PlayerControlManager.cs
@@ -19,6 +19,10 @@ public class PlayerControlManager : MonoBehaviour
     /// The jump event for object to subscribe
     /// </summary>
     public UnityEvent<bool> JumpEvent;
+
+    public UnityEvent SabotageEvent;
+
+    public UnityEvent InteractEvent;
     /// <summary>
     /// Get movement input
     /// </summary>
@@ -39,5 +43,25 @@ public class PlayerControlManager : MonoBehaviour
         if (i_input.started) return;
         Jump= i_input.performed;
         JumpEvent.Invoke(Jump);
+    }
+
+    /// <summary>
+    /// Get Sabotage input
+    /// </summary>
+    /// <param name="i_input"></param>
+    public void GetSabotage(CallbackContext i_input)
+    {
+        if (i_input.started) return;
+        if (i_input.performed) SabotageEvent.Invoke();
+    }
+
+    /// <summary>
+    /// Get interact input
+    /// </summary>
+    /// <param name="i_input"></param>
+    public void GetInteract(CallbackContext i_input)
+    {
+        if (i_input.started) return;
+        if (i_input.performed) InteractEvent.Invoke();
     }
 }
